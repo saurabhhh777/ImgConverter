42ff2267dd8ef3af570a480637d33fdae4ed0cd2
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-02-14 11:14:03 +0530

    Navbar and default Page add on Home Page

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../NavbarPage/Navbar.tsx";
+import Pngtojpg from "../Tools/Pngtojpg.tsx";
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Pngtojpg/>
     </div>
+    
   )
 }
 
